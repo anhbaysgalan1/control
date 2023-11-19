7fbb25b54071365604845e3d51126ee257b116cf
Ankhbayasgalan Davaadorj <31855776+anhbaysgalan1@users.noreply.github.com>
2023-11-20 06:44:35 +0800

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   retries: 2,
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost",
   },
 } as PlaywrightTestConfig;
 export default config;
